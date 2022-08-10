4ab953ee60cc4363ce6d4b5daa0a4a04f92263eb
sjhansen3 <stephenjhansen8@gmail.com>
2022-08-09 22:40:14 -0700

    Make auto player mover slower

--- Assets/PlayerAutoMover.cs
+++ Assets/PlayerAutoMover.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerAutoMover : MonoBehaviour
 {
     private Transform rb;
-    public float speed = 1;
+    public float speed = .02f;
     private float movementX;
     private float movementY;
     private float time;
-    public float frequency = 1.0f;
+    public float frequency = 0.001f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,9 +28,13 @@ public class PlayerAutoMover : MonoBehaviour
 
     private void Update() {
         time += Time.deltaTime;
+        // Debug.Log("time: "+time);
+
         float sine = Mathf.Sin(time * frequency);
-        movementX = Mathf.Sin(time * frequency);;
-        movementY = 3 * Mathf.Cos(time * frequency);
+        movementX = sine/5;
+        movementY = 3.0f * Mathf.Cos(time * frequency);
+
+        // Debug.Log("movementX "+movementX+"movementY "+movementY);
         gameObject.transform.Translate(0.0f, 0.0f, movementY*speed);
         gameObject.transform.Rotate(0.0f,movementX*speed,0.0f);
 
